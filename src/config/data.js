746d395441fe2713ad038c4a1c767fc2f6e2445e
Pablo Henrique Penha Silva <pablohpsilva@Pablos-MacBook-Pro.local>
2017-10-29 15:11:54 -0200

    all app data is on data

--- src/config/data.js
+++ src/config/data.js
@@ -1,4 +1,10 @@
 export default {
+  contactLinks: [
+    { link: true, href: '#facebook', small: false, icon: '' },
+    { link: true, href: '#twitter', small: false, icon: '' },
+    { link: true, href: '#instagram', small: false, icon: '' },
+    { link: true, href: '#slack', small: false, icon: '' }
+  ],
   toolbarLinks: [
     { text: 'Objetivo', link: true, href: '#objetivo', small: false },
     { text: 'Quem somos', link: true, href: '#quem-somos', small: false },
@@ -6,10 +12,6 @@ export default {
     { text: 'Contato', link: true, href: '#contato', small: false },
     { text: 'Inscreva-se', link: false, small: true }
   ],
-  welcomeContainer: {
-    h1: { id: 'welcome', text: 'FrontInUDI 2017' },
-    h2: { text: 'Chegamos na melhor cidade mineira' }
-  },
   talkCards: [
     {
       title: 'Nome da Talk',
@@ -31,5 +33,40 @@ export default {
       text: 'React: o framework onipresente - Hipsters',
       author: { name: 'Fernando Garcia', contact: 'https://google.com' }
     }
-  ]
+  ],
+  organization: [
+    { name: 'Fernando Garcia', profession: 'UX/UI Design e frontend', picture: '' },
+    { name: 'Fernando Garcia', profession: 'UX/UI Design e frontend', picture: '' },
+    { name: 'Fernando Garcia', profession: 'UX/UI Design e frontend', picture: '' },
+    { name: 'Fernando Garcia', profession: 'UX/UI Design e frontend', picture: '' }
+  ],
+  welcomeContainer: {
+    h1: { id: 'welcome', text: 'FrontInUDI 2017' },
+    h2: { text: 'Chegamos na melhor cidade mineira' }
+  },
+  learnShareContainer: {
+    h1: { id: 'objetivo', text: 'Aprender, compartilhar e se divertir' },
+    p: { text: 'FrontInUDI foi criado com o propósito de reunir interessados em desenvolvimento Frontend e outras tecnologias relacionadas, desde entusiastas às mais experientes para realizarmos discussões, palestras e workshops com o intuito de compartilhar conhecimento e experiências.' }
+  },
+  whoWeAreContainer: {
+    h1: { id: 'quem-somos', text: 'Quem somos' },
+    p: { text: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua' }
+  },
+  frontTalkContainer: {
+    h1: { id: 'front-talk', text: 'Front-Talk' },
+    p: { text: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua' }
+  },
+  contactContainer: {
+    h1: { id: 'contato', text: 'Quer patrocinar? Dúvidas? Sugestões?' },
+    h2: { text: 'Escreva pra gente' },
+    email: { text: 'frontinudi@gmail.com' },
+    span: { text: 'Acompanhe o FrontInUDI:' }
+  },
+  registerContainer: {
+    input: { text: 'Informe seu email e fique atualizado' }
+    button: { text: 'Cadastrar email' }
+  }
+  footerContainer: {
+    p: { text: 'com amor para a comunidade', icon: '' }
+  }
 }
